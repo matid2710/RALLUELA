9e7bee1192bc78212109400d6167d27a1e79e199
matid2710 <mdobisky@gmail.com>
2025-09-18 11:33:46 -0300

    TERMINE

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+import fs from "fs";
+
+let ordentxt = fs.readFileSync("orden.txt", "utf-8")
+
+
+let partes = ordentxt.split("\r\n")
+console.log(partes)
+//let orden = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136];
+let texto =  "";
+
+for (let i = 0; i < partes.length; i++){
+    texto = fs.readFileSync("Cuento/parte" + partes[i] + ".txt", "utf-8");
+    console.log(texto);
+}
+fs.writeFileSync("La autopista del sur.txt", texto)
+ 
